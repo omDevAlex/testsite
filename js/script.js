2d8a4ea52c4b2fa4794063b8198d24e70c36c935
Alex Stepanov <stepanov22@gmail.com>
2021-11-22 16:44:31 +0200

    +leagues

--- js/script.js
+++ js/script.js
@@ -1,18 +1,45 @@
+/*jshint esversion: 6 */
+
 window.addEventListener('DOMContentLoaded', () => {
+
+  // change theme
+
+  const toggleCheckbox = document.querySelector('.toggle');
+
+  function toggleTheme() {    
+    if (toggleCheckbox.checked) {
+      localStorage.setItem( 'dark', true);
+    } else {
+      localStorage.setItem( 'dark', false);
+    }
+  }
+  function theme() {
+    let darkMode = document.querySelector('#dark-mode');
+    if (localStorage.dark === 'true') {
+      document.body.classList.add('dark'); 
+      darkMode.checked = true;
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }
+
+  theme();
+  
+
+  toggleCheckbox.addEventListener('click', toggleTheme);
+  toggleCheckbox.addEventListener('click', theme);
 
   // modal
 
   const modal = document.querySelector('.modal'),
-        modalOpen = document.querySelectorAll('[data-modal]'),
+        modalOpen = document.querySelector('[data-modal]'),
         modalClose = document.querySelector('[data-close]');
 
     function toggleModal() {
         modal.classList.toggle('show');      
     }
     
-    modalOpen.forEach(btn => {
-        btn.addEventListener('click', toggleModal);
-    });
+    modalOpen.addEventListener('click', toggleModal);
 
     modalClose.addEventListener('click', toggleModal);
 
@@ -73,7 +100,7 @@ window.addEventListener('DOMContentLoaded', () => {
           }
       });
   }
-  });
+  });  
 
   //rait tabs
 
@@ -106,36 +133,7 @@ window.addEventListener('DOMContentLoaded', () => {
           }
       });
   }
-  });
-
-  // change theme
-
-  const toggleCheckbox = document.querySelector('.toggle');
-
-  function toggleTheme() {    
-    if (toggleCheckbox.checked) {
-      localStorage.setItem( 'dark', true);
-    } else {
-      localStorage.setItem( 'dark', false);
-    }
-  };
-  function theme() {
-    let darkMode = document.querySelector('#dark-mode');
-    if (localStorage.dark === 'true') {
-      console.log('tratata');
-      document.body.classList.add('dark'); 
-      darkMode.checked = true;
-    } else {
-      console.log(localStorage.dark);
-      document.body.classList.remove('dark');
-    }
-  };
-
-  theme();
-  
-
-  toggleCheckbox.addEventListener('click', toggleTheme);
-  toggleCheckbox.addEventListener('click', theme);
+  });    
 
   // lang list
 
@@ -205,31 +203,36 @@ window.addEventListener('DOMContentLoaded', () => {
         filterItems = document.querySelectorAll('.news_block'),
         listItems = document.querySelectorAll('.filter_item');
 
-  function filter() {
-    filterList.addEventListener('click', e => {
-      const targetId = e.target.dataset.id;
-      const target = e.target;
-
-      if (target.classList.contains('filter_item')) {
-        listItems.forEach(listItem => listItem.classList.remove('filter_active'));
-        target.classList.add('filter_active');
-      }
-
-      switch(targetId) {
-        case 'all':
-          getItems('news_block');
-          break;
-        case 'popular':
-          getItems(targetId);
-          break;
-        case 'newest':
-          getItems(targetId);
-          break;
-      }
-    });
+  if (filterList !==null) {
+    function filter() {
+      filterList.addEventListener('click', e => {
+        const targetId = e.target.dataset.id;
+        const target = e.target;
+  
+        if (target.classList.contains('filter_item')) {
+          listItems.forEach(listItem => listItem.classList.remove('filter_active'));
+          target.classList.add('filter_active');
+        }
+  
+        switch(targetId) {
+          case 'all':
+            getItems('news_block');
+            break;
+          case 'popular':
+            getItems(targetId);
+            break;
+          case 'newest':
+            getItems(targetId);
+            break;
+        }
+      });
+    }
   }
-
-  filter();
+  
+  if (filterList !==null) {
+    filter();
+  }
+  
 
   function getItems(className) {
     filterItems.forEach(item =>{
@@ -240,5 +243,307 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     });
   }
+
+  // tourneys filter
+
+  const filterTurnirsCs = document.querySelector('.tourneys_tabcontent_filter_cs'),
+        filterBloksTurnirCs = document.querySelectorAll('.turnir_bloks_cs'),
+        listTurnirItemsCs = document.querySelectorAll('.turnir_item_cs');
+
+  if (filterTurnirsCs !==null) {
+    function filter() {
+      filterTurnirsCs.addEventListener('click', e => {
+        const targetId = e.target.dataset.id;
+        const target = e.target;
+  
+        if (target.classList.contains('turnir_item_cs')) {
+          listTurnirItemsCs.forEach(listItem => listItem.classList.remove('turnir_active_cs'));
+          target.classList.add('turnir_active_cs');
+        }
+  
+        switch(targetId) {
+          case 'active':
+            getItems(targetId);
+            break;
+          case 'completing':
+            getItems(targetId);
+            break;
+          case 'forthcoming':
+            getItems(targetId);
+            break;
+        }
+      });
+    }
+  
+    filter();
+  
+
+    function getItems(className) {
+      filterBloksTurnirCs.forEach(item =>{
+        if (item.classList.contains(className)) {
+          item.style.display = 'flex';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    }
+  }
+
+  const filterTurnirsDt = document.querySelector('.tourneys_tabcontent_filter_dt'),
+        filterBloksTurnirDt = document.querySelectorAll('.turnir_bloks_dt'),
+        listTurnirItemsDt = document.querySelectorAll('.turnir_item_dt');
+
+  if (filterTurnirsDt !==null) {
+    function filter() {
+      filterTurnirsDt.addEventListener('click', e => {
+        const targetId = e.target.dataset.id;
+        const target = e.target;
+  
+        if (target.classList.contains('turnir_item_dt')) {
+          listTurnirItemsDt.forEach(listItem => listItem.classList.remove('turnir_active_dt'));
+          target.classList.add('turnir_active_dt');
+        }
+  
+        switch(targetId) {
+          case 'active':
+            getItems(targetId);
+            break;
+          case 'completing':
+            getItems(targetId);
+            break;
+          case 'forthcoming':
+            getItems(targetId);
+            break;
+        }
+      });
+    }
+  
+    filter();
+  
+
+    function getItems(className) {
+      filterBloksTurnirDt.forEach(item =>{
+        if (item.classList.contains(className)) {
+          item.style.display = 'flex';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    }
+  }
+
+  const filterTurnirsFf = document.querySelector('.tourneys_tabcontent_filter_ff'),
+        filterBloksTurnirFf = document.querySelectorAll('.turnir_bloks_ff'),
+        listTurnirItemsFf = document.querySelectorAll('.turnir_item_ff');
+
+  if (filterTurnirsFf !==null) {
+    function filter() {
+      filterTurnirsFf.addEventListener('click', e => {
+        const targetId = e.target.dataset.id;
+        const target = e.target;
+  
+        if (target.classList.contains('turnir_item_ff')) {
+          listTurnirItemsFf.forEach(listItem => listItem.classList.remove('turnir_active_ff'));
+          target.classList.add('turnir_active_ff');
+        }
+  
+        switch(targetId) {
+          case 'active':
+            getItems(targetId);
+            break;
+          case 'completing':
+            getItems(targetId);
+            break;
+          case 'forthcoming':
+            getItems(targetId);
+            break;
+        }
+      });
+    }
+  
+    filter();
+  
+
+    function getItems(className) {
+      filterBloksTurnirFf.forEach(item =>{
+        if (item.classList.contains(className)) {
+          item.style.display = 'flex';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    }
+  }
+
+
+  //_________
+
+  const filterLeaguesCs = document.querySelector('.leagues_tabcontent_filter_cs'),
+        filterBloksLeagueCs = document.querySelectorAll('.league_bloks_cs'),
+        listLeagueItemsCs = document.querySelectorAll('.league_item_cs');
+
+  if (filterLeaguesCs !==null) {
+    function filter() {
+      filterLeaguesCs.addEventListener('click', e => {
+        const targetId = e.target.dataset.id;
+        const target = e.target;
+  
+        if (target.classList.contains('league_item_cs')) {
+          listLeagueItemsCs.forEach(listItem => listItem.classList.remove('league_active_cs'));
+          target.classList.add('league_active_cs');
+        }
+  
+        switch(targetId) {
+          case 'fxf':
+            getItems(targetId);
+            break;
+          case 'txt':
+            getItems(targetId);
+            break;
+          case 'oxo':
+            getItems(targetId);
+            break;
+        }
+      });
+    }
+  
+    filter();
+  
+
+    function getItems(className) {
+      filterBloksLeagueCs.forEach(item =>{
+        if (item.classList.contains(className)) {
+          item.style.display = 'flex';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    }
+  }
+
+  const filterLeaguesDt = document.querySelector('.leagues_tabcontent_filter_dt'),
+        filterBloksLeagueDt = document.querySelectorAll('.league_bloks_dt'),
+        listLeagueItemsDt = document.querySelectorAll('.league_item_dt');
+
+  if (filterLeaguesDt !==null) {
+    function filter() {
+      filterLeaguesDt.addEventListener('click', e => {
+        const targetId = e.target.dataset.id;
+        const target = e.target;
+  
+        if (target.classList.contains('league_item_dt')) {
+          listLeagueItemsDt.forEach(listItem => listItem.classList.remove('league_active_dt'));
+          target.classList.add('league_active_dt');
+        }
+  
+        switch(targetId) {
+          case 'fxf':
+            getItems(targetId);
+            break;
+          case 'txt':
+            getItems(targetId);
+            break;
+          case 'oxo':
+            getItems(targetId);
+            break;
+        }
+      });
+    }
+  
+    filter();
+  
+
+    function getItems(className) {
+      filterBloksLeagueDt.forEach(item =>{
+        if (item.classList.contains(className)) {
+          item.style.display = 'flex';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    }
+  }
+
+  const filterLeaguesFf = document.querySelector('.leagues_tabcontent_filter_ff'),
+        filterBloksLeagueFf = document.querySelectorAll('.league_bloks_ff'),
+        listLeagueItemsFf = document.querySelectorAll('.league_item_ff');
+
+  if (filterLeaguesFf !==null) {
+    function filter() {
+      filterLeaguesFf.addEventListener('click', e => {
+        const targetId = e.target.dataset.id;
+        const target = e.target;
+  
+        if (target.classList.contains('league_item_ff')) {
+          listLeagueItemsFf.forEach(listItem => listItem.classList.remove('league_active_ff'));
+          target.classList.add('league_active_ff');
+        }
+  
+        switch(targetId) {
+          case 'fxf':
+            getItems(targetId);
+            break;
+          case 'txt':
+            getItems(targetId);
+            break;
+          case 'oxo':
+            getItems(targetId);
+            break;
+        }
+      });
+    }
+  
+    filter();
+  
+
+    function getItems(className) {
+      filterBloksLeagueFf.forEach(item =>{
+        if (item.classList.contains(className)) {
+          item.style.display = 'flex';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    }
+  }
+
+  
+
+  // game tabs
+
+  const tabsParentGame = document.querySelector('.tabheader_game__items'),
+        tabsGame = document.querySelectorAll('.tabheader_game__item'),
+        tabsContentGame = document.querySelectorAll('.tabcontent_game');
+
+  if (tabsParentGame !==null) {
+    function hideTabGame() {
+      tabsContentGame.forEach(item => {
+          item.classList.remove('tabshow_game');
+      });
+      tabsGame.forEach(item => {
+          item.classList.remove('tabheader_game__item_active');
+      });
+    }
+  
+    function showTabGame(i) {
+      tabsContentGame[i].classList.add('tabshow_game');
+      tabsGame[i].classList.add('tabheader_game__item_active');
+    }
+  
+    tabsParentGame.addEventListener('click', (e) => {
+    const target = e.target;
+  
+    if (target && target.classList.contains('tabheader_game__item')) {
+        tabsGame.forEach((item, i) => {
+            if (target == item) {
+                hideTabGame();
+                showTabGame(i);
+            }
+        });
+    }
+    });
+  }
+
+  
 
 });
